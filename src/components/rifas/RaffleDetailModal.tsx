2e42968ea5e256d61d1419ecc3dc721203627426
Diego Quintero <diego.quintero@fravega.com.ar>
2025-11-04 03:31:02 -0300

    fix RaffleDetailModal

--- src/components/rifas/RaffleDetailModal.tsx
+++ src/components/rifas/RaffleDetailModal.tsx
@@ -321,10 +321,6 @@ export default function RaffleDetailModal({ raffle, onClose, onTicketPurchased }
                 </>
               ) : (
                 <>
-                  <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor">
-                    <path d="M9 12l2 2 4-4"/>
-                    <path d="M12 2a10 10 0 1 0 10 10"/>
-                  </svg>
                   Comprar boletos
                 </>
               )}
